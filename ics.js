987faad688e02a1b8d2af7c04d2a2421ab63f44f
sacha.bertschi <sacha.bertschi@YOTSUBA>
2017-04-04 08:20:48 +0200

    Correction selon ESLint.

--- ics.js
+++ ics.js
@@ -4,12 +4,12 @@
 var ics = function() {
     'use strict';
 
-    if (navigator.userAgent.indexOf('MSIE') > -1 && navigator.userAgent.indexOf('MSIE 10') == -1) {
+    if (navigator.userAgent.indexOf('MSIE') > -1 && navigator.userAgent.indexOf('MSIE 10') === -1) {
         console.log('Unsupported Browser');
         return;
     }
 
-    var SEPARATOR = (navigator.appVersion.indexOf('Win') !== -1) ? '\r\n' : '\n';
+    var SEPARATOR = navigator.appVersion.indexOf('Win') !== -1 ? '\r\n' : '\n';
     var calendarEvents = [];
     var calendarStart = [
         'BEGIN:VCALENDAR',
@@ -41,6 +41,7 @@ var ics = function() {
          * @param  {string} location    Location of event
          * @param  {string} begin       Beginning date of event
          * @param  {string} stop        Ending date of event
+         * @returns {string} a calendarEvent
          */
         'addEvent': function(subject, description, location, begin, stop) {
             // I'm not in the mood to make these optional... So they are all required
@@ -51,30 +52,30 @@ var ics = function() {
                 typeof stop === 'undefined'
             ) {
                 return false;
-            };
+            }
 
             //TODO add time and time zone? use moment to format?
             var start_date = new Date(begin);
             var end_date = new Date(stop);
 
-            var start_year = ("0000" + (start_date.getFullYear().toString())).slice(-4);
-            var start_month = ("00" + ((start_date.getMonth() + 1).toString())).slice(-2);
-            var start_day = ("00" + ((start_date.getDate()).toString())).slice(-2);
-            var start_hours = ("00" + (start_date.getHours().toString())).slice(-2);
-            var start_minutes = ("00" + (start_date.getMinutes().toString())).slice(-2);
-            var start_seconds = ("00" + (start_date.getMinutes().toString())).slice(-2);
+            var start_year = ("0000" + start_date.getFullYear().toString()).slice(-4);
+            var start_month = ("00" + (start_date.getMonth() + 1).toString()).slice(-2);
+            var start_day = ("00" + (start_date.getDate()).toString()).slice(-2);
+            var start_hours = ("00" + start_date.getHours().toString()).slice(-2);
+            var start_minutes = ("00" + start_date.getMinutes().toString()).slice(-2);
+            var start_seconds = ("00" + start_date.getMinutes().toString()).slice(-2);
 
-            var end_year = ("0000" + (end_date.getFullYear().toString())).slice(-4);
-            var end_month = ("00" + ((end_date.getMonth() + 1).toString())).slice(-2);
-            var end_day = ("00" + ((end_date.getDate()).toString())).slice(-2);
-            var end_hours = ("00" + (end_date.getHours().toString())).slice(-2);
-            var end_minutes = ("00" + (end_date.getMinutes().toString())).slice(-2);
-            var end_seconds = ("00" + (end_date.getMinutes().toString())).slice(-2);
+            var end_year = ("0000" + end_date.getFullYear().toString()).slice(-4);
+            var end_month = ("00" + (end_date.getMonth() + 1).toString()).slice(-2);
+            var end_day = ("00" + (end_date.getDate()).toString()).slice(-2);
+            var end_hours = ("00" + end_date.getHours().toString()).slice(-2);
+            var end_minutes = ("00" + end_date.getMinutes().toString()).slice(-2);
+            var end_seconds = ("00" + end_date.getMinutes().toString()).slice(-2);
 
             // Since some calendars don't add 0 second events, we need to remove time if there is none...
             var start_time = '';
             var end_time = '';
-            if (start_minutes + start_seconds + end_minutes + end_seconds != 0) {
+            if (start_minutes + start_seconds + end_minutes + end_seconds !== 0) {
                 start_time = 'T' + start_hours + start_minutes + start_seconds;
                 end_time = 'T' + end_hours + end_minutes + end_seconds;
             }
@@ -102,14 +103,15 @@ var ics = function() {
          * Download calendar using the saveAs function from filesave.js
          * @param  {string} filename Filename
          * @param  {string} ext      Extention
+         * @returns {string} a calendar
          */
         'download': function(filename, ext) {
             if (calendarEvents.length < 1) {
                 return false;
             }
 
-            ext = (typeof ext !== 'undefined') ? ext : '.ics';
-            filename = (typeof filename !== 'undefined') ? filename : 'calendar';
+            ext = typeof ext !== 'undefined' ? ext : '.ics';
+            filename = typeof filename !== 'undefined' ? filename : 'calendar';
             var calendar = calendarStart + SEPARATOR + calendarEvents.join(SEPARATOR) + calendarEnd;
 
             var blob;
